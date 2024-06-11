c4bd0685f8c6e31a20f99d0c213b299a479a5ba2
sujeong87 <josujeong87@gmail.com>
2024-06-11 13:27:01 +0900

    QNA 리스트 - 0611 수정

--- Final_Project/src/main/java/com/kh/yoonsart/QNA/model/dao/QNADao.java
+++ Final_Project/src/main/java/com/kh/yoonsart/QNA/model/dao/QNADao.java
@@ -1,17 +1,38 @@
 package com.kh.yoonsart.QNA.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.yoonsart.QNA.model.vo.QNA;
+import com.kh.yoonsart.common.model.vo.PageInfo;
 
 @Repository
 public class QNADao {
 	
-	// 게시글 갯수
+	// 문의글 총 갯수
 	public int selectListCount(SqlSessionTemplate sqlSession) {
 			
 			return sqlSession.selectOne("qnaMapper.selectListCount");
 	}
 	
+	// 목록 조회
+	public ArrayList<QNA> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+			
+		int limit = pi.getBoardLimit();
+		int offset = (pi.getCurrentPage() - 1) * limit;
+			
+		RowBounds rowBounds = new RowBounds(offset, limit);
+			
+		return (ArrayList)sqlSession.selectList("qnaMapper.selectList", null, rowBounds);
+	}
+	
+	// 문의글 작성 
+	public int insertQNA(SqlSessionTemplate sqlSession, QNA q) {
+		return sqlSession.insert("qnaMapper.insertQNA", q);
+	}
 	
 	
 	
